d5f362afea32bb87f8d94dbef9f888ef6ffd1f71
pseudoureka <messiah7179@gmail.com>
2025-02-05 23:41:48 +0900

    PATCH users/:id API에서 userPreference 객체 수정하는 코드 추가

--- app.js
+++ app.js
@@ -103,10 +103,20 @@ app.patch(
   asyncHandler(async (req, res) => {
     assert(req.body, PatchUser);
     const { id } = req.params;
+    const { userPreference, ...userFields } = req.body;
     const user = await prisma.user.update({
       where: { id },
-      data: req.body,
+      data: {
+        ...userFields,
+        userPreference: {
+          update: userPreference,
+        },
+      },
+      include: {
+        userPreference: true,
+      },
     });
+
     res.send(user);
   })
 );
