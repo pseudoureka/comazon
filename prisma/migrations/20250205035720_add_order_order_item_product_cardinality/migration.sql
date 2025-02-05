e8f18a45c2814867dc070d93f03e9b3f75984e46
pseudoureka <messiah7179@gmail.com>
2025-02-05 13:10:30 +0900

    model 수정, onDelete 속성 적용

---
+++ prisma/migrations/20250205035720_add_order_order_item_product_cardinality/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `orderId` to the `OrderItem` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `productId` to the `OrderItem` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "userId" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "OrderItem" ADD COLUMN     "orderId" TEXT NOT NULL,
+ADD COLUMN     "productId" TEXT NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
